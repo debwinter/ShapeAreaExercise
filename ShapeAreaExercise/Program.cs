f1f594135e80c89bd742eba939bdb2bbc4ef15bb
Debra Winter <deb@debwinter.com>
2024-01-12 10:35:33 -0500

    added method Circle

--- ShapeAreaExercise/Program.cs
+++ ShapeAreaExercise/Program.cs
@@ -3,7 +3,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+    }
+
+    // Method to calculate area of a circle
+    public static double Circle(double radius)
+    {
+        double area = radius * 2 * Math.PI;
+        return area;
     }
 }
 
